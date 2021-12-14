7e4d8e6f2126f17097fc311838be2ed20d6ecf41
Adrian Nads <marcelonads@gmail.com>
2021-12-14 21:12:26 +0800

    added a javascript

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,2 +1,38 @@
 "use strict"
 
+const softRed = 'hsl(0, 93%, 68%)';
+const desaturatedRed = 'hsl(0, 36%, 70%)'
+
+const setState = (emailBorderColor, visibility) => {
+
+    const emailField = document.querySelector('input[type="email"]')
+    const errorIcon = document.querySelector('.error-icon')
+    const errorMessage = document.querySelector('.error-message')
+
+    emailField.style.border = `1px solid ${emailBorderColor}`;
+    errorIcon.style.visibility = visibility;
+    errorMessage.style.visibility = visibility;
+}
+document.querySelector('form').addEventListener('submit', (e) => {
+
+    e.preventDefault();
+})
+
+document.querySelector('button').addEventListener('click', () => {
+
+    const emailField = document.querySelector('input[type="email"]')
+    const errorIcon = document.querySelector('.error-icon')
+    const errorMessage = document.querySelector('.error-message')
+
+    if (emailField.value === '') {
+        setState(softRed, 'visible')
+    }
+        
+}) 
+
+document.querySelector('input[type="email"]').addEventListener('input', () => {
+
+    setState(desaturatedRed, 'hidden')
+})
+
+console.log('hello')
